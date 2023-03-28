c74b69b01b668a598efb8f1c95e076142c2e533f
Casu3475 <106447335+Casu3475@users.noreply.github.com>
2023-03-28 08:15:40 +0200

    mailing

---
+++ mail.php
@@ -0,0 +1,49 @@
+<?php
+
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if(isset($_POST['send'])){
+    $name = htmlentities($_POST['name']);
+    $email = htmlentities($_POST['email']);
+    $subject = htmlentities($_POST['subject']);
+    $message = htmlentities($_POST['message']);
+
+    $mail = new PHPMailer(true);
+    // $mail -> SMTPDebug = SMTP::DEBUG_SERVER;
+
+try {
+    // SMTP configuration
+    $mail -> isSMTP();
+    $mail -> Host = "smtp.gmail.com"; // set the SMTP server to send through
+    $mail -> SMTPAuth = true; // enable SMTP authentication
+    $mail -> Username =''; // your gmail address
+    $mail -> Password ='';  // your App password
+    $mail -> Port = 465; // TCP port to connect to
+    // $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // enable TLS encryption
+    $mail -> SMTPSecure = 'ssl';
+
+    // Sender and recipient settings
+    $mail -> setFrom($email, $name);
+    $mail -> addAddress("");
+    $mail->addReplyTo($email, $name);
+
+    // Content and formatting
+    $mail -> isHTML(true);
+    $mail -> Subject = ("$email ($subject)");
+    $mail -> Body = $message;
+
+    // Send the message
+    $mail -> send(); 
+
+    header ("Location: ./response.php");
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+}
+?>
